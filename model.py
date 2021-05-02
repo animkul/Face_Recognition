caa6be4e453dc01ddee0206a0d9d2b699c25b4ef
animkul <83549124+animkul@users.noreply.github.com>
2021-05-02 22:18:10 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,3 +1,7 @@
+# -----------------------------------------------------------------------------------------
+# Code taken from https://github.com/iwantooxxoox/Keras-OpenFace (with minor modifications)
+# -----------------------------------------------------------------------------------------
+
 from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
 from keras.layers.core import Lambda, Flatten, Dense
 from keras.layers.normalization import BatchNormalization
